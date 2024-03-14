de54096c9317a7085c5a8be1bd9769a6c7e08a33
Pedro Henrique <122654777+PedroHSGAraujo@users.noreply.github.com>
2024-03-14 09:46:42 -0300

    atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+var soma1 = 1;
+var soma2 = 2;
+var resultSoma = soma1 + soma2;
+
+var subtracao1 = 2;
+var subtracao2 = 1;
+var resultSub = subtracao1 - subtracao2;
+
+var multi1 = 2;
+var multi2 = 3;
+var resultMulti = multi1 * multi2;
+
+var divisao1 = 4;
+var divisao2 = 2;
+var resultDivisao = divisao1 / divisao2;
+
+console.log("O resultado da divisão entre " + divisao1 + " e " + divisao2 + " é: " + resultDivisao);
+console.log("O resultado da multiplicação entre " + multi1 + " e " + multi2 + " é: " + resultMulti);
+console.log("O resultado da soma entre " + soma1 + " e " + soma2 + " é: " + resultSoma);
+console.log("O resultado da soma entre " + subtracao1 + " e " + subtracao2 + " é: " + resultSub);
